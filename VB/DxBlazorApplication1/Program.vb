bffa54a17da4f2d6184486716c348e82997140fe
DevExpressExampleBot <fakeEmail>
2022-09-29 18:59:48 +0400

    autogenerate VB

---
+++ VB/DxBlazorApplication1/Program.vb
@@ -0,0 +1,239 @@
+Imports Microsoft.AspNetCore.Components
+' Add services to the container.
+' The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+Imports Microsoft.AspNetCore.Components.Web ' TODO: Error SkippedTokensTrivia ';'
+
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia '}'
+' TODO: Error SkippedTokensTrivia ')'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' Configure the HTTP request pipeline.
+' TODO: Error SkippedTokensTrivia 'if'
+' TODO: Error SkippedTokensTrivia ')'
+' TODO: Error SkippedTokensTrivia '{'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia '}'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+' TODO: Error SkippedTokensTrivia ';'
+ ''' Cannot convert FieldDeclarationSyntax, System.ArgumentOutOfRangeException: Exception of type 'System.ArgumentOutOfRangeException' was thrown.
+''' Parameter name: member
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.GetMemberContext(MemberDeclarationSyntax member)
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitFieldDeclaration(FieldDeclarationSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+''' 
+''' 
+''' var builder = WebApplication.CreateBuilder(args);
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.Services.AddRazorPages' at character 210
+''' 
+''' 
+''' Input:
+''' 
+''' // Add services to the container.
+''' builder.Services.AddRazorPages
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 240
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.Services.AddServerS...' at character 245
+''' 
+''' 
+''' Input:
+''' builder.Services.AddServerSideBlazor
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 281
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.Services.AddDevExpr...' at character 286
+''' 
+''' 
+''' Input:
+''' builder.Services.AddDevExpressBlazor
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 322
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.Services.Configure<...' at character 327
+''' 
+''' 
+''' Input:
+''' builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '(options ' at character 400
+''' 
+''' 
+''' Input:
+''' (options => {
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'options.BootstrapVersion' at character 419
+''' 
+''' 
+''' Input:
+'''     options.BootstrapVersion = 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'DevExpress.Blazor.Bootstrap...' at character 446
+''' 
+''' 
+''' Input:
+''' DevExpress.Blazor.BootstrapVersion.v5;
+''' });
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.WebHost.UseWebRoot' at character 491
+''' 
+''' 
+''' Input:
+''' builder.WebHost.UseWebRoot
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '(' at character 517
+''' 
+''' 
+''' Input:
+''' ("wwwroot");
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'builder.WebHost.UseStaticWe...' at character 531
+''' 
+''' 
+''' Input:
+''' builder.WebHost.UseStaticWebAssets
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 565
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert FieldDeclarationSyntax, System.ArgumentOutOfRangeException: Exception of type 'System.ArgumentOutOfRangeException' was thrown.
+''' Parameter name: member
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.GetMemberContext(MemberDeclarationSyntax member)
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitFieldDeclaration(FieldDeclarationSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+''' var app = builder.Build();
+''' 
+''' // Configure the HTTP request pipeline.
+''' if 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '(' at character 644
+''' 
+''' 
+''' Input:
+''' (!
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.Environment.IsDevelopment' at character 646
+''' 
+''' 
+''' Input:
+''' app.Environment.IsDevelopment
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 675
+''' 
+''' 
+''' Input:
+''' ())
+''' {
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.UseExceptionHandler' at character 687
+''' 
+''' 
+''' Input:
+'''     app.UseExceptionHandler
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '(' at character 710
+''' 
+''' 
+''' Input:
+''' ("/Error");
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.UseHsts' at character 860
+''' 
+''' 
+''' Input:
+'''     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+'''     app.UseHsts
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 871
+''' 
+''' 
+''' Input:
+''' ();
+''' }
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.UseHttpsRedirection' at character 879
+''' 
+''' 
+''' Input:
+''' app.UseHttpsRedirection
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 902
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.UseStaticFiles' at character 909
+''' 
+''' 
+''' Input:
+''' 
+''' app.UseStaticFiles
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 927
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.UseRouting' at character 934
+''' 
+''' 
+''' Input:
+''' 
+''' app.UseRouting
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 948
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.MapBlazorHub' at character 957
+''' 
+''' 
+''' Input:
+''' 
+''' 
+''' app.MapBlazorHub
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 973
+''' 
+''' 
+''' Input:
+''' ();
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.MapFallbackToPage' at character 978
+''' 
+''' 
+''' Input:
+''' app.MapFallbackToPage
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '(' at character 999
+''' 
+''' 
+''' Input:
+''' ("/_Host");
+''' 
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in 'app.Run' at character 1014
+''' 
+''' 
+''' Input:
+''' 
+''' app.Run
+'''   ''' Cannot convert IncompleteMemberSyntax, CONVERSION ERROR: Conversion for IncompleteMember not implemented, please report this issue in '()' at character 1021
+''' 
+''' 
+''' Input:
+''' ();
+''' 
